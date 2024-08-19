0044e481a84fbea1360d454ea16df9713257d7a4
H51M14\Student <Student@gmail.com>
2024-08-19 16:36:12 +0700

    CodingChallenge4

--- Part1/CodingChallenge4.js
+++ Part1/CodingChallenge4.js
@@ -14,3 +14,15 @@
 // Hints:
 // §  To calculate 20% of a value, simply multiply it by 20/100 = 0.2
 // §  Value X is between 50 and 300, if it's >= 50 && <= 300
+
+var bill = 275
+var tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
+
+bill = 40
+tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
+
+bill = 430
+tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
